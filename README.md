0c935f8b9f59c51f60b13e7d2aec13400ab06a6a
Albert <153892855+Albrt78@users.noreply.github.com>
2024-02-27 19:32:52 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+## Table of contents
+
+-   [Overview](#overview)
+-   [The challenge](#the-challenge)
+-   [Screenshot](#screenshot)
+-   [Links](#links)
+-   [Built with](#built-with)
+-   [Author](#author)
+
+## Overview
+
+This challenge is designed based on the provided template from Frontend Mentor which help us improve our coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+-   View the optimal layout for the site depending on their device's screen size
+-   See hover states for all interactive elements on the page
+
+### Screenshot
+
+![](./src/Fylo_landing_page_with_two_column_layout.png)
+
+### Links
+
+-   Solution URL: [https://github.com/Albrt78/frontend_mentor_project/tree/main/Fylo%20Landing%20Page]
+-   Live Site URL: [https://65b5f18bcdadaf21d7c69765--luminous-bunny-12b656.netlify.app/]
+
+### Built with
+
+-   Semantic HTML5 markup
+-   CSS custom properties
+-   Mobile-first workflow
+-   JavaScript
+-   [Bootstrap5](https://getbootstrap.com) - CSS framework
+
+## Author
+
+-   Frontend Mentor - [@Albrt78](https://www.frontendmentor.io/profile/Albrt78)
---
+++ script.js
@@ -0,0 +1,67 @@
+// Hamburger Menu button
+const hamburgerIcon = document.getElementById("hamburger-menu");
+const hamburgerMenu = document.querySelector(".navbar");
+
+// alert
+const alert = document.getElementById("alert");
+const formInput = document.getElementById("input");
+const getStartedButton = document.getElementById("button");
+
+// newsletter alert
+const newsletterAlert = document.getElementById("newsletter-alert");
+const newsletterInput = document.getElementById("newsletter-form");
+const newsletterButton = document.getElementById("newsletter-button");
+
+// For email validation
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+// Register Validation
+function inputValidation() {
+    if (!formInput.value.match(emailRegex)) {
+        alert.style.visibility = "visible";
+        formInput.style.border = "3px dotted red";
+    } else {
+        alert.style.visibility = "hidden";
+        formInput.style.border = "3px dotted var(--Moderate-Cyan)";
+    }
+}
+
+formInput.addEventListener("input", () => {
+    inputValidation();
+});
+
+getStartedButton.onclick = (e) => {
+    inputValidation();
+    e.preventDefault();
+};
+
+// Newsletter Validation
+function newsletterValidation() {
+    if (!newsletterInput.value.match(emailRegex)) {
+        newsletterAlert.style.visibility = "visible";
+        newsletterInput.style.border = "3px dotted red";
+    } else {
+        newsletterAlert.style.visibility = "hidden";
+        newsletterInput.style.border = "3px dotted var(--Moderate-Cyan)";
+    }
+}
+
+newsletterInput.addEventListener("input", () => {
+    newsletterValidation();
+});
+
+newsletterButton.onclick = (e) => {
+    newsletterValidation();
+    e.preventDefault();
+};
+
+// Hamburger Menu Event
+hamburgerIcon.onclick = () => {
+    hamburgerMenu.classList.toggle("active");
+};
+
+document.addEventListener("click", (e) => {
+    if (!hamburgerIcon.contains(e.target) && hamburgerMenu.contains(e.target)) {
+        hamburgerMenu.classList.remove("active");
+    }
+});
